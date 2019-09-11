76322f8dcf8d8b83de88143dba78620ba0bdaaee
cansu <cansubidici@gmail.com>
2019-09-11 10:58:34 +0300

    #15-empty-file-check

--- merger.py
+++ merger.py
@@ -11,7 +11,7 @@ class Window(tk.Tk):
     def __init__(self):
         # TODO https://stackoverflow.com/questions/19196130/select-all-in-a-tkinter-listbox
         super().__init__()
-        self.turn = 1
+        self.turn = 0
         self.files = []
         self.output = pd.DataFrame()
 
@@ -49,36 +49,47 @@ class Window(tk.Tk):
             for file in self.files:
                 file = file.rsplit("/", 1)[1]
                 self.listbox_list_of_files.insert(END, file)
+
+
         else:
             for file in self.files:
                 self.listbox_list_of_files.insert(END, file)
         self.turn = not self.turn
 
     def select_file(self):
-        files_select = filedialog.askopenfilenames(filetypes=(("xlsx or xls files", "*.xls*"), ("xlsx or xls", ".xls*")))
+        try:
+            files_select = filedialog.askopenfilenames(filetypes=[("Default Excel file", "*.xlsx"), ("Excel file 97-2003", "*.xls")])
+
+        except FileNotFoundError:
+            messagebox.showerror("Error Message", "File not found")
+        except Exception as e:
+            messagebox.showerror('Awww', 'Unknown Error Occurred.')
+
         for file in files_select:
             self.files.append(file)
             file = file.rsplit("/", 1)[1]
-            # items = self.listbox_list_of_files.curselection()
-            # print(items)
+
             self.listbox_list_of_files.insert(END, file)
 
     def merge_file(self):
-        self.output = merge(self.listbox_list_of_files.get(0, END))
-        f = filedialog.asksaveasfilename(defaultextension='.xlsx',
-                                         filetypes=(("xlsx files support", "*.xlsx"), ("xls files support", ".xls")))
-        if not f.split('/')[-1].endswith('.xlsx'):
-            messagebox.showerror("Hata Mesajı", "Sadece xlsx ve xls türündeki dosyalar")
-            return
-        try:
-            writer = pd.ExcelWriter(f, engine='xlsxwriter')
-            self.output.to_excel(writer, sheet_name='RESULT')
-            writer.save()
-        except Exception as e:
-            messagebox.showerror('Awww', 'Bir hata oluştu.')
+        self.output = merge(self.files)
+        if self.files.__len__() == 0:
+            messagebox.showerror("Error Message", "You did not select source file")
+        else:
+            f = filedialog.asksaveasfilename(defaultextension='.xlsx',
+                                         filetypes=[("Default Excel file", "*.xlsx"), ("Excel file 97-2003", "*.xls")])
 
-        # TODO show completed window
-        print("completed")
+            if not f.rsplit("/", 1)[1].endswith('.xlsx' or '.xls'):
+                messagebox.showerror("Error Message", "Only xlsx and xls File Types")
+                return
+            self.listbox_list_of_files.insert(END, "Your Selected Target Files " + f.rsplit("/", 1)[1])
+            try:
+                writer = pd.ExcelWriter(f, engine='xlsxwriter')
+                self.output.to_excel(writer)
+                writer.save()
+            except Exception as e:
+                messagebox.showerror('Awww', 'Unknown Error Occurred.')
+            self.listbox_list_of_files.insert(END, "Merge Completed to " + f.rsplit("/", 1)[1])
 
 
 if __name__ == '__main__':
