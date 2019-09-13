63367856be5d4102a55342071a71b4dd96d79f3b
cansu <cansubidici@gmail.com>
2019-09-13 09:39:54 +0300

    Canvas transparent background added

--- merger.py
+++ merger.py
@@ -3,8 +3,10 @@ import tkinter as tk
 from tkinter import filedialog
 from tkinter import messagebox
 from tkinter import Listbox
+from tkinter import Label
 from utils import merge
 import pandas as pd
+from PIL import Image, ImageTk
 
 
 class Window(tk.Tk):
@@ -15,31 +17,34 @@ class Window(tk.Tk):
         self.files = []
         self.output = pd.DataFrame()
 
-        self.img = tk.PhotoImage(file='assets/background.gif')
-
-
-        self.w = self.img.width()
-        self.h = self.img.height()
-        self.label_background = tk.Label(image=self.img, width=800, height=1000)
+        self.img_window_background = tk.PhotoImage(file='assets/background.gif')
+        self.w = self.img_window_background.width()
+        self.h = self.img_window_background.height()
+        self.label_background = tk.Label(image=self.img_window_background, width=1200, height=600)
         self.label_background.place(relx=0.00, rely=0.00, relheight=1, relwidth=1)
 
-        self.geometry('690x490+600+150')
+        self.img = tk.PhotoImage(file='assets/banner.png')
+        self.can = tk.Canvas(self, width=790, height=100)
+        self.can.grid()
+        self.can.create_image(400, 70, image=self.img)
+
+        self.geometry('790x490+600+150')
         self.resizable(width=False, height=False)
         self.configure()
 
         self.title("Excel File Merge Program")
 
         self.button_files = tk.Button(text="Select Files", bg='White', command=self.select_file)
-        self.button_files.place(relx=0.65, rely=0.4, relheight=0.1, relwidth=0.15)
+        self.button_files.place(relx=0.62, rely=0.35, relheight=0.1, relwidth=0.15)
 
-        self.button_merge = tk.Button(text="Merge", width=200, pady=20, bg='White', command=self.merge_file)
-        self.button_merge.place(relx=0.1, rely=0.8, relheight=0.13, relwidth=0.8)
+        self.button_merge = tk.Button(text="Merge", width=200, pady=20, bg='White', command=self.merge_file, font=('Helvetica', '30'))
+        self.button_merge.place(relx=0.05, rely=0.8, relheight=0.13, relwidth=0.9)
 
         self.listbox_list_of_files = Listbox(listvariable=self.files)
-        self.listbox_list_of_files.place(relx=0.20, rely=0.3, relheight=0.3, relwidth=0.4)
+        self.listbox_list_of_files.place(relx=0.1, rely=0.3, relheight=0.35, relwidth=0.5)
 
         self.button_filepath = tk.Button(text="Show Full Path", bg='White', command=self.change_path_mode)
-        self.button_filepath.place(relx=0.3, rely=0.62, relheight=0.05, relwidth=0.20)
+        self.button_filepath.place(relx=0.12, rely=0.67, relheight=0.06, relwidth=0.20)
 
     def change_path_mode(self):
         self.button_filepath.config(text='Show Full Path' if self.turn else 'Hide Path')
@@ -83,12 +88,12 @@ class Window(tk.Tk):
                 messagebox.showerror("Error Message", "Only xlsx and xls File Types")
                 return
             try:
-                writer = pd.ExcelWriter(f, engine='xlsxwriter')
+                writer = pd.ExcelWriter(f, engine='xlsx_writer')
                 self.output.to_excel(writer)
                 writer.save()
                 self.listbox_list_of_files.delete(0, END)
                 messagebox.showinfo("", "File Merge Completed")
-                self.tk.call('wm', 'iconphoto', self._w, tk.PhotoImage(file='assets/successful_icon.gif'))
+                self.tk.call('wm', 'icon_photo', self._w, tk.PhotoImage(file='assets/successful_icon.gif'))
 
             except Exception as e:
                 print(e)
